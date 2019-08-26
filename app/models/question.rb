628fd4a2e81237ff9d0cac0d4349ad708b62e99c
vitalyatn <stolyarov.vitalij@list.ru>
2019-08-26 17:47:03 +1000

    homework

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
-	has_many :answers, dependent: :destroy
-	validates :title, :body, presence: true
+  
+  has_many :answers, dependent: :destroy
+  validates :title, :body, presence: true
 
 end
